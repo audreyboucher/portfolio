3220465f0d96e3aa420cff359752100f505e7cab
Audrey Boucher <audrey@abtasty.com>
2025-04-17 13:56:26 +0200

    about me section responsive

--- src/components/sections/AboutMeSection/AboutMeSection.tsx
+++ src/components/sections/AboutMeSection/AboutMeSection.tsx
@@ -50,7 +50,7 @@ const AboutMeSection = () => {
   return (
     <Section anchor='about' background='secondary' containerClassName={styles.container}>
       <>
-        <aside  className={styles.carrouselImageContainer}>
+        <aside  className={classNames(styles.carrouselImageContainer, styles.desktopOnly)}>
           <ImageCarrousel
             images={slides.map(({ cover, keywords }, index) => ({
               path: cover.map((image) => require(`./images/${ image }`)),
@@ -64,14 +64,28 @@ const AboutMeSection = () => {
 
         <article className={styles.textContainer}>
           <h4 className={styles.title}>About me</h4>
-          <ProgressBar value={progress} animationDuration={parseInt(INTERVAL_DURATION)} />
+          <ProgressBar value={progress} animationDuration={parseInt(INTERVAL_DURATION)} className={styles.desktopOnly} />
+          <ul className={classNames(styles.dots, styles.mobileOnly)}>
+            {
+              slides.map((_, index) =>
+                <li
+                  className={classNames(styles.dot, { [styles.selected]: index === selectedItem })}
+                  key={index}
+                  onClick={() => onSelectAction(index)}
+                ></li>
+              )
+            }
+          </ul>
           <ul className={styles.carrouselTextContainer}>
-            {slides.map(({ keywords, description }: Slide, index) => (
+            {slides.map(({ keywords, description, cover }: Slide, index) => (
               <li
                 className={classNames(styles.carrouselText, { [styles.selected]: index === selectedItem })}
                 key={index}
                 onClick={() => onSelectAction(index)}
               >
+                <figure className={styles.mobileOnly}>
+                  <img src={require(`./images/${ cover[0] }`)} alt={keywords.join(' & ')} />
+                </figure>
                 <h5>{keywords.map((keyword, i) => <span key={i}>{ keyword }</span>)}</h5>
                 <div>
                   {description.map((paragraph, i) => <p key={i}>{ paragraph }</p>)}
